a7e4b19e08868f62e6fd2bf48f88926a92529ca3
Ednesor <edgar.funes2001@gmail.com>
2023-05-23 19:03:33 -0300

    style: navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,20 @@ import style from "./Navbar.module.css";
 import Logo from "../../../public/Logo.png";
 import Image from 'next/image';
 import { Link } from "react-scroll";
+import { AiOutlineMenu } from "react-icons/ai"
+import { GiCancel } from "react-icons/gi"
 
 export default function Navbar() {
 
     const [scrolled, setScrolled] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(0);
+    const [menu, setMenu] = useState(false);
 
     useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
         const handleScroll = () => {
             if (window.scrollY > 0) {
                 setScrolled(true);
@@ -18,20 +26,35 @@ export default function Navbar() {
         };
 
         window.addEventListener('scroll', handleScroll);
+        window.addEventListener('resize', handleResize);
 
         return () => {
             window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('resize', handleResize);
         };
     }, []);
+
+    const selectStyle = () => {
+        let menuStyle = {};
+        if (scrolled && windowWidth > 900) {
+            menuStyle = { boxShadow: "0 0 15px black" };
+        } else if (windowWidth <= 900) {
+            menuStyle = { boxShadow: "0 0 15px black" };
+        }
+        if (menu && windowWidth <= 900) {
+            menuStyle = {
+                boxShadow: "0 0 15px black",
+                top: "0px"
+            };
+        }
+
+        return menuStyle;
+    }
 
     return (
         <nav
             className={style.container}
-            style={
-                scrolled ?
-                    {boxShadow: "0 0 15px black"} :
-                    null
-            }
+            style={selectStyle()}
 
         >
             <div className={style.logoContainer}>
@@ -39,64 +62,77 @@ export default function Navbar() {
                     className={style.image}
                     src={Logo}
                     alt='Best Logo Ever'
+                    style={
+                        (menu && windowWidth <= 900) ? {
+                            width: "0px"
+                        } : null
+                    }
                 />
                 <p className={style.name}>EDNESOR</p>
+                {windowWidth <= 900 ?
+                    menu ?
+                        <GiCancel className={style.icon} onClick={() => setMenu(!menu)} /> :
+                        <AiOutlineMenu className={style.icon} onClick={() => setMenu(!menu)} />
+                    : null
+                }
             </div>
-            <ul className={style.links}>
-                <li>
-                    <Link
-                        activeClass={"active"}
-                        to="home"
-                        spy={true}
-                        smooth={true}
-                        offset={50}
-                        duration={500}
-                        delay={100}
-                    >
-                        HOME
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        activeClass={"active"}
-                        to="services"
-                        spy={true}
-                        smooth={true}
-                        offset={50}
-                        duration={500}
-                        delay={100}
-                    >
-                        SERVICIOS
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        activeClass={"active"}
-                        to="projects"
-                        spy={true}
-                        smooth={true}
-                        offset={50}
-                        duration={500}
-                        delay={100}
-                    >
-                        PROYECTOS
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        activeClass={"active"}
-                        to="contact"
-                        spy={true}
-                        smooth={true}
-                        offset={50}
-                        duration={500}
-                        delay={100}
-                    >
-                        CONTACTO
-                    </Link>
-                </li>
-            </ul>
-            <button className={style.download}>DESCARGAR CV</button>
+            <div className={style.menu}>
+                <ul className={style.links}>
+                    <li>
+                        <Link
+                            activeClass={"active"}
+                            to="home"
+                            spy={true}
+                            smooth={true}
+                            offset={50}
+                            duration={500}
+                            delay={100}
+                        >
+                            HOME
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            activeClass={"active"}
+                            to="services"
+                            spy={true}
+                            smooth={true}
+                            offset={50}
+                            duration={500}
+                            delay={100}
+                        >
+                            SERVICIOS
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            activeClass={"active"}
+                            to="projects"
+                            spy={true}
+                            smooth={true}
+                            offset={50}
+                            duration={500}
+                            delay={100}
+                        >
+                            PROYECTOS
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            activeClass={"active"}
+                            to="contact"
+                            spy={true}
+                            smooth={true}
+                            offset={50}
+                            duration={500}
+                            delay={100}
+                        >
+                            CONTACTO
+                        </Link>
+                    </li>
+                </ul>
+                <button className={style.download}>DESCARGAR CV</button>
+            </div>
         </nav>
     )
 }
---
+++ src/pages/index.jsx
@@ -0,0 +1,147 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Layout from "../components/Layout/Layout"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1 id='home'>Pagina principal</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1 id='services'>Pagina principal</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1 id='projects'>Pagina principal</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1 id='contact'>Pagina principal</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+    </Layout>
+  )
+}
