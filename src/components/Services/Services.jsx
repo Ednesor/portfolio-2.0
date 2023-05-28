49e700362e444f7abca0db61898c3d615c29a474
Ednesor <edgar.funes2001@gmail.com>
2023-05-28 20:39:02 -0300

    style: add titles

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import style from "./Services.module.css"
-import {AiOutlineDesktop} from "react-icons/ai"
-import {BiCartAlt} from "react-icons/bi"
-import {RiArticleLine} from "react-icons/ri"
+import { AiOutlineDesktop } from "react-icons/ai"
+import { BiCartAlt } from "react-icons/bi"
+import { RiArticleLine } from "react-icons/ri"
 
 export default function Services() {
   return (
-    <section className={style.container} id='services'>
-      <div className={style.card}>
-        <AiOutlineDesktop className={style.icon} />
-        <p className={style.title}>Sitio Web</p>
-        <p className={style.body}>Transformo tus ideas en realidad con diseños web modernos y funcionales que destacan la identidad de tu marca. Impulsa tu presencia en línea y atrae a tu audiencia con una experiencia de usuario cautivadora.</p>
+    <section className={style.container}>
+      <div className={style.tContainer}>
+        <p className={style.t1}>Mis servicios</p>
+        <p className={style.t2}>¿Que puedo hacer?</p>
       </div>
-      <div className={style.card}>
-        <BiCartAlt className={style.icon} />
-        <p className={style.title}>E-commerce</p>
-        <p className={style.body}>Abre las puertas al éxito en el mundo del comercio electrónico con una tienda en línea atractiva y fácil de usar. Potencia tus ventas, administra tus productos y llega a clientes de todo el mundo. ¡Convierte tus visitantes en clientes satisfechos!</p>
-      </div>
-      <div className={style.card}>
-        <RiArticleLine className={style.icon} />
-        <p className={style.title}>Blog</p>
-        <p className={style.body}>Crea un espacio personalizado para compartir tus pensamientos, conocimientos y pasiones. Con un diseño atractivo y funciones interactivas, tu blog se convertirá en un destino favorito para los amantes de la lectura y una poderosa herramienta para promocionar tu negocio. ¡Deja que tus palabras brillen en la web!</p>
+      <div className={style.cards} id='services'>
+        <div className={style.card}>
+          <AiOutlineDesktop className={style.icon} />
+          <p className={style.title}>Sitio Web</p>
+          <p className={style.body}>Transformo tus ideas en realidad con diseños web modernos y funcionales que destacan la identidad de tu marca. Impulsa tu presencia en línea y atrae a tu audiencia con una experiencia de usuario cautivadora.</p>
+        </div>
+        <div className={style.card}>
+          <BiCartAlt className={style.icon} />
+          <p className={style.title}>E-commerce</p>
+          <p className={style.body}>Abre las puertas al éxito en el mundo del comercio electrónico con una tienda en línea atractiva y fácil de usar. Potencia tus ventas, administra tus productos y llega a clientes de todo el mundo. ¡Convierte tus visitantes en clientes satisfechos!</p>
+        </div>
+        <div className={style.card}>
+          <RiArticleLine className={style.icon} />
+          <p className={style.title}>Blog</p>
+          <p className={style.body}>Crea un espacio personalizado para compartir tus pensamientos, conocimientos y pasiones. Con un diseño atractivo y funciones interactivas, tu blog se convertirá en un destino favorito para los amantes de la lectura y una poderosa herramienta para promocionar tu negocio. ¡Deja que tus palabras brillen en la web!</p>
+        </div>
       </div>
     </section>
   )
