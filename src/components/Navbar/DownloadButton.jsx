d4fbe4ce7c7438fe0690de6d89039bcf81360348
Ednesor <edgar.funes2001@gmail.com>
2023-05-29 19:07:38 -0300

    fix download cv

--- src/components/Navbar/DownloadButton.jsx
+++ src/components/Navbar/DownloadButton.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import style from "./Navbar.module.css";
 
-const DownloadButton = () => {
-  const handleDownload = () => {
-    fetch('https://docs.google.com/document/d/1M55TABWoVgQft8v0Q1Zt8TZy4lOmN1i4/edit?usp=sharing&ouid=113194324438980732698&rtpof=true&sd=true')
-      .then((response) => response.blob())
-      .then((blob) => {
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = 'CV Edgardo Funes.pdf'; 
-        link.click();
-      });
-  };
+const DownloadButton = () => {  
 
   return (
-    <button className={style.download} onClick={handleDownload}>DESCARGAR CV</button>
+    <a 
+    className={style.download} 
+    href={"/Edgardo-Funes-CV.pdf"}
+    download={"Edgardo-Funes-CV"}
+    target='_blank'
+    rel='noreferrer'
+    >DESCARGAR CV</a>
   );
 };
 
