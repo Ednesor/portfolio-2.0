c10106a82437911a60bb62e564eae066c6233114
Ednesor <77041457+Ednesor@users.noreply.github.com>
2023-06-29 20:18:42 -0300

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import style from "./Projects.module.css"
 import Image from 'next/image'
 import P1 from "./Project Img/P1.png";
+import P2 from "./Project Img/P2.png";
 import Link from 'next/link';
 
 export default function Projects() {
@@ -18,8 +19,17 @@ export default function Projects() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          <Image className={style.image} src={P1} alt="Una imagen" />
-          <p className={style.title}>Tarjeta de invitacion</p>
+          <Image className={style.image} src={P1} alt="Tarjeta de invitacion" />
+          <p className={style.title}>Tarjeta de invitación</p>
+        </Link>
+        <Link
+          href={"https://github.com/Ednesor/peluqueria-canina"}
+          className={style.card}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <Image className={style.image} src={P2} alt="Peluqueria canina" />
+          <p className={style.title}>Peluquería canina</p>
         </Link>
       </div>
     </section>
